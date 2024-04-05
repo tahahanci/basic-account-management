43fdccc0d5cfef974f92dd44037179876fc245ea
Taha Hancı <taha.hanci@boun.edu.tr>
2024-04-06 01:16:26 +0300

    models created.

---
+++ src/main/java/org/hncdev/accountmanagement/model/Transaction.kt
@@ -0,0 +1,29 @@
+package org.hncdev.accountmanagement.model
+
+import jakarta.persistence.*
+import org.hibernate.annotations.GenericGenerator
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+data class Transaction(
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String?,
+    val transactionType: TransactionType? = TransactionType.INITIAL,
+    val amount: BigDecimal?,
+    val transactionDate: LocalDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "account_id", nullable = false)
+    val account: Account?
+
+) {
+
+}
+
+enum class TransactionType {
+    INITIAL, TRANSFER
+}
