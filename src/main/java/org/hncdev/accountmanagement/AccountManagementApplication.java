37bd0074b5ffc32659b7fc047a332dc39b679e90
Taha Hancı <taha.hanci@boun.edu.tr>
2024-04-06 16:55:46 +0300

    accountmanagementapplication changed

--- src/main/java/org/hncdev/accountmanagement/AccountManagementApplication.java
+++ src/main/java/org/hncdev/accountmanagement/AccountManagementApplication.java
@@ -1,13 +1,31 @@
 package org.hncdev.accountmanagement;
 
+import org.hncdev.accountmanagement.model.Customer;
+import org.hncdev.accountmanagement.repository.AccountRepository;
+import org.hncdev.accountmanagement.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+
 @SpringBootApplication
-public class AccountManagementApplication {
+public class AccountManagementApplication implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    public AccountManagementApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountManagementApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer = customerRepository
+                .save(new Customer("", "Taha", "Hanci", new HashSet<>()));
+        System.out.println(customer.getId());
+    }
 }
